30d73a1876461a91e6baedd35125a265dc30ac8f
joshbaker624 <joshbakergf@users.noreply.github.com>
2025-08-12 20:17:57 -0400

    Fix development setup for Raspberry Pi
    - Added concurrently and wait-on for proper dev server management
    - Added webpack-dev-server to devDependencies
    - Updated build path for production mode
    - Dev script now properly starts webpack before Electron

--- src/main/index.js
+++ src/main/index.js
@@ -33,7 +33,7 @@ function createWindow() {
     mainWindow.loadURL('http://localhost:3000');
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+    mainWindow.loadFile(path.join(__dirname, '../../build/index.html'));
   }
 
   mainWindow.on('closed', () => {
